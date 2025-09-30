1d37e817244159cfdb7b83e5ec5f5ee1e8684053
Mahdi Dadashi <mahdidadashi@gmail.com>
2025-09-30 10:55:25 +0330

    can test

---
+++ STM32_Class_S9/STM32F4_CAN/MyApp/myapp.c
@@ -0,0 +1,376 @@
+#include "main.h"
+#include "gpio.h"
+#include "usart.h"
+#include "adc.h"
+#include "tim.h"
+#include "stdio.h"
+#include "stdbool.h"
+#include "can.h"
+
+
+
+#ifdef __GNUC__
+  /* With GCC, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1, 0xFFFF); 
+
+  return ch;
+}
+
+
+
+void test_led(void)
+{
+  while (1)
+  {
+    
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(100);
+    
+  }
+}
+
+void test_uart(void)
+{
+  while (1)
+  {
+    
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(100);
+    HAL_UART_Transmit(&huart3,"Hello\r\n",7,100);
+    
+  }
+}
+
+
+void test_printf(void)
+{
+  while (1)
+  {
+    
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(100);
+    printf("Hello %d\r\n",5);
+    
+  }
+}
+
+long map(long x, long in_min, long in_max, long out_min, long out_max) 
+{ 
+    return (x - in_min) * (out_max - out_min + 1) / (in_max - in_min + 1) + out_min;
+}
+
+void test_adc(void)
+{
+  int adc_voltage_mv = 0;
+  int adc_val = 0;
+  
+  while (1)
+  {
+    
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(100);
+    
+     /*##-3- Start the conversion process #######################################*/  
+    if(HAL_ADC_Start(&hadc1) != HAL_OK)
+    {
+      /* Start Conversation Error */
+      Error_Handler();
+    }
+    
+    /*##-4- Wait for the end of conversion #####################################*/  
+     /*  Before starting a new conversion, you need to check the current state of 
+          the peripheral; if it’s busy you need to wait for the end of current
+          conversion before starting a new one.
+          For simplicity reasons, this example is just waiting till the end of the 
+          conversion, but application may perform other tasks while conversion 
+          operation is ongoing. */
+    HAL_ADC_PollForConversion(&hadc1, 10);
+    
+    /* Check if the continuous conversion of regular channel is finished */
+    if((HAL_ADC_GetState(&hadc1) & HAL_ADC_STATE_EOC_REG) == HAL_ADC_STATE_EOC_REG)
+    {
+      /*##-5- Get the converted value of regular channel  ######################*/
+      adc_val = HAL_ADC_GetValue(&hadc1);
+    }
+  
+    adc_voltage_mv = map(adc_val, 0, 4095, 0, 3300);
+    printf("adc_voltage_mv:%d mV\r\n",adc_voltage_mv);
+    
+  }
+}
+
+
+
+//void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
+//{
+//  int adc_voltage_mv = 0;
+//  int adc_val = 0;
+//  /* Get the converted value of regular channel */
+//  adc_val = HAL_ADC_GetValue(AdcHandle);
+//  adc_voltage_mv = map(adc_val, 0, 4095, 0, 3300);
+//  printf("adc_voltage_mv:%d mV\r\n",adc_voltage_mv);
+//}
+
+
+
+
+void test_adc_int(void)
+{
+
+  
+  while (1)
+  {
+    
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(1000);
+    
+      /*##-3- Start the conversion process and enable interrupt ##################*/  
+    if(HAL_ADC_Start_IT(&hadc1) != HAL_OK)
+    {
+      /* Start Conversation Error */
+      Error_Handler();
+    }
+  
+
+    
+  }
+}
+
+uint16_t uhADCxConvertedValue[10000] = {0};
+
+bool adc_convert_status=false;
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle)
+{
+  HAL_GPIO_TogglePin(LOGIC_GPIO_Port,LOGIC_Pin);
+  adc_convert_status =  true;
+}
+
+void test_adc_dma(void)
+{
+ int adc_voltage_mv = 0;
+  
+
+  
+  while (1)
+  {
+    
+    HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(1000);
+   
+        /*##-3- Start the conversion process and enable interrupt ##################*/
+  /* Note: Considering IT occurring after each number of ADC conversions      */
+  /*       (IT by DMA end of transfer), select sampling time and ADC clock    */
+  /*       with sufficient duration to not create an overhead situation in    */
+  /*        IRQHandler. */ 
+    adc_convert_status=false;
+    HAL_GPIO_TogglePin(LOGIC_GPIO_Port,LOGIC_Pin);
+
+  if(HAL_ADC_Start_DMA(&hadc1, (uint32_t*)uhADCxConvertedValue, 10000) != HAL_OK)
+  {
+    /* Start Conversation Error */
+    Error_Handler(); 
+  }
+  
+    adc_voltage_mv = map(uhADCxConvertedValue[0], 0, 4095, 0, 3300);
+    printf("adc_voltage_mv:%d mV\r\n",adc_voltage_mv);
+
+  }
+}
+
+
+void test_adc_pwm(void)
+{
+  int pwm_percent = 0;
+  int adc_val = 0;
+  
+  
+  __HAL_TIM_SetAutoreload(&htim1,100-1);
+  __HAL_TIM_PRESCALER(&htim1,((84000000/1000)/100)+1);
+  __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1,40);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  
+  while (1)
+  {
+    
+    //HAL_GPIO_TogglePin(LED_B_GPIO_Port,LED_B_Pin);
+    HAL_Delay(100);
+    
+     /*##-3- Start the conversion process #######################################*/  
+    if(HAL_ADC_Start(&hadc1) != HAL_OK)
+    {
+      /* Start Conversation Error */
+      Error_Handler();
+    }
+    
+    /*##-4- Wait for the end of conversion #####################################*/  
+     /*  Before starting a new conversion, you need to check the current state of 
+          the peripheral; if it’s busy you need to wait for the end of current
+          conversion before starting a new one.
+          For simplicity reasons, this example is just waiting till the end of the 
+          conversion, but application may perform other tasks while conversion 
+          operation is ongoing. */
+    HAL_ADC_PollForConversion(&hadc1, 10);
+    
+    /* Check if the continuous conversion of regular channel is finished */
+    if((HAL_ADC_GetState(&hadc1) & HAL_ADC_STATE_EOC_REG) == HAL_ADC_STATE_EOC_REG)
+    {
+      /*##-5- Get the converted value of regular channel  ######################*/
+      adc_val = HAL_ADC_GetValue(&hadc1);
+    }
+  
+    pwm_percent = map(adc_val, 0, 4095, 0, 100);
+    printf("pwm:%d %%\r\n",pwm_percent);
+    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1,pwm_percent);
+    
+  }
+}
+
+
+CAN_TxHeaderTypeDef   TxHeader;
+CAN_RxHeaderTypeDef   RxHeader;
+uint8_t               TxData[8];
+uint8_t               RxData[8];
+uint32_t              TxMailbox;
+
+
+static void CAN_Config_500k_42m(void)
+{
+  CAN_FilterTypeDef  sFilterConfig;
+
+  /*##-1- Configure the CAN peripheral #######################################*/
+  hcan1.Instance = CAN1;
+  hcan1.Init.Prescaler = 6;
+  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_11TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
+  hcan1.Init.TimeTriggeredMode = DISABLE;
+  hcan1.Init.AutoBusOff = DISABLE;
+  hcan1.Init.AutoWakeUp = DISABLE;
+  hcan1.Init.AutoRetransmission = DISABLE;
+  hcan1.Init.ReceiveFifoLocked = DISABLE;
+  hcan1.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /*##-2- Configure the CAN Filter ###########################################*/
+  sFilterConfig.FilterBank = 0;
+  sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+  sFilterConfig.FilterIdHigh = 0x0000;
+  sFilterConfig.FilterIdLow = 0x0000;
+  sFilterConfig.FilterMaskIdHigh = 0x0000;
+  sFilterConfig.FilterMaskIdLow = 0x0000;
+  sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
+  sFilterConfig.FilterActivation = ENABLE;
+  sFilterConfig.SlaveStartFilterBank = 14;
+
+  if (HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig) != HAL_OK)
+  {
+    /* Filter configuration Error */
+    Error_Handler();
+  }
+
+  /*##-3- Start the CAN peripheral ###########################################*/
+  if (HAL_CAN_Start(&hcan1) != HAL_OK)
+  {
+    /* Start Error */
+    Error_Handler();
+  }
+
+  /*##-4- Activate CAN RX notification #######################################*/
+  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+  {
+    /* Notification Error */
+    Error_Handler();
+  }
+
+  /*##-5- Configure Transmission process #####################################*/
+  TxHeader.StdId = 0x321;
+  TxHeader.ExtId = 0x01;
+  TxHeader.RTR = CAN_RTR_DATA;
+  TxHeader.IDE = CAN_ID_STD;
+  TxHeader.DLC = 3;
+  TxHeader.TransmitGlobalTime = DISABLE;
+}
+
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+  /* Get RX message */
+  if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK)
+  {
+    /* Reception Error */
+    Error_Handler();
+  }
+  
+  
+  printf("ID :%x Len:%d Data:%x\r\n",RxHeader.StdId,RxHeader.DLC,RxData[0]);
+
+//  /* Display LEDx */
+//  if ((RxHeader.StdId == 0x321) && (RxHeader.IDE == CAN_ID_STD) && (RxHeader.DLC == 2))
+//  {
+//    LED_Display(RxData[0]);
+//    ubKeyNumber = RxData[0];
+//  }
+}
+
+void test_can(void)
+{
+  
+  CAN_Config_500k_42m();
+  int ubKeyNumber =0;
+  while(1)
+  {
+//        TxData[0] = ubKeyNumber++;
+//        TxData[1] = 0xAD;
+    
+        TxData[0] = 0x11;
+        TxData[1] = 0x22;
+        TxData[2] = 0x33;
+        
+        /* Start the Transmission process */
+        if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+        {
+          /* Transmission request Error */
+          Error_Handler();
+        }
+        
+         TxData[0] = 0x10;
+        TxData[1] = 0x20;
+        TxData[2] = 0x30;
+        if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+        {
+          /* Transmission request Error */
+          Error_Handler();
+        }
+         HAL_Delay(100);
+  }
+  
+}
+
+void MyApp(void)
+{
+   
+  //test_led();
+  //test_uart();
+  //test_printf();
+  //test_adc();
+  //test_adc_int();
+  //test_adc_dma();
+  //test_adc_pwm();
+  test_can();
+  
+}
---
+++ STM32_Class_S9/STM32F4_CAN/MyApp/myapp.h
@@ -0,0 +1,2 @@
+
+void MyApp(void);
